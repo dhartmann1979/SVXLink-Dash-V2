17ea44ec134ab376a3ff7bf9ce3b93754d2248dd
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2023-01-04 23:06:14 +0000

    m

--- dtmf.php
+++ dtmf.php
@@ -135,7 +135,7 @@ if ($MENUBUTTON=="BOTTOM") {
 include_once __DIR__."/include/buttons.php"; }
 ?>
 <!--- Please do not remove copyright info -->
-<center><span title="Dashboard" style="font: 7pt arial, sans-serif;">SvxLink Dashboard ©  SP2ONG, SP0DZ <?php $cdate=date("Y"); if ($cdate > "2021") {$cdate="2021-".date("Y");} echo $cdate; ?>
+<center><span title="Dashboard" style="font: 7pt arial, sans-serif;">SvxLink Dashboard ©  SP2ONG, SP0DZ, G4NAB <?php $cdate=date("Y"); if ($cdate > "2021") {$cdate="2021-".date("Y");} echo $cdate; ?>
 	</div>
 </div>
 </fieldset>
