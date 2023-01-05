0a851de694f3cce2d1654b13e7fe13caee1d265a
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2023-01-05 09:27:20 +0000

    m

--- audio.php
+++ audio.php
@@ -67,7 +67,7 @@ $svxConfigFile = '/etc/svxlink/svxlink.conf';
          $callsign = $svxconfig['ReflectorLogic']['CALLSIGN'];}
     else { $callsign="N0CALL";}
 ?>
-<span style="font-size: 32px;letter-spacing:4px;font-family: &quot;sans-serif&quot;, sans-serif;font-weight:500;color:DarkOrange"><?php echo $callsign; ?></span>
+<span style="font-size: 32px;letter-spacing:4px;font-family: &quot;sans-serif&quot;, sans-serif;font-weight:500;color:PaleBlue"><?php echo $callsign; ?></span>
 <p style="margin-top:0px;margin-bottom:0px;">
 <span style="font-size: 30px;font-family: 'sans-serif', 'Helvetica Neue', Helvetica, Arial, sans-serif;letter-spacing: 3px;font-weight: 600;background: #3083b8;"><?php echo $fmnetwork; ?></span>
 </p></center>
