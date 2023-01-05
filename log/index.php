c859a77f4b3e78de119f2c7ea8a3e3349a86e00e
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2023-01-05 09:32:23 +0000

    m

--- log/index.php
+++ log/index.php
@@ -111,7 +111,7 @@ if (isset($_POST['btnLog']))
         //$sAconn = $_POST['sAconn'];
         //$password = $_POST['password'];
         //exec('nmcli dev wifi rescan');
-        $command = "tail -l /var/log/svxlink.log |tac 2>&1";
+        $command = "tail -l /var/log/svxlink.log";
         exec($command,$screen,$retval);
 }
 
