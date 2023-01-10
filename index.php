04d89dada350566f0eace648de400de50a51f52b
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2023-01-10 16:17:46 +0000

    m

--- index.php
+++ index.php
@@ -61,6 +61,7 @@ else { $callsign="N0CALL";
 <div class="content"><center>
 <div style="margin-top:0px;">
 </div></center>
+</div>
 <!--<?php
 if (isProcessRunning('node')) {
 echo '&nbsp;&nbsp;<button class="button link" onclick="playAudioToggle(8080, this)"><b>&nbsp;&nbsp;&nbsp;<img src=images/speaker.png alt="" style="vertical-align:middle">&nbsp;&nbsp;RX Monitor&nbsp;&nbsp;&nbsp;</b></button><br><br>';
