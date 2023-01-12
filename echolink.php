b89042d6095adbedb2cc21f2ac3a0fcbe6141b84
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2023-01-12 09:12:06 +0000

    m

--- echolink.php
+++ echolink.php
@@ -12,7 +12,7 @@ $svxConfigFile = '/etc/svxlink/svxlink.conf';
 else { $callsign="N0CALL"; 
        $fmnetwork="no registered";
 	}
-
+$elConfigFile = "/etc/svxlink/svxlink.d/ModuleEchoLink.conf";
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
