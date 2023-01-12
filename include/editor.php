c7831f51c2ab655c1f5119c945027dd01b87261c
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2023-01-12 15:11:20 +0000

    m

--- include/editor.php
+++ include/editor.php
@@ -21,7 +21,7 @@ if($if="metarinfo"){
 // check if form has been submitted
 //$filename = by choice;
 
-$url = "editor.php";
+$url = "include/editor.php";
 {
     // save the text contents
     file_put_contents($file, $_POST['text']);
