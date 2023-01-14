e0aa391cc0ac37f670920654a789df67a5255304
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2023-01-14 19:57:28 +0000

    m

--- include/config.php
+++ include/config.php
@@ -12,17 +12,40 @@ define("FULLACCESS_OUSIDE", "Full Access Outside");
 define("EL_NODE_NR",'$EL_node');
 
 
-$svxConfigFile = '/etc/svxlink/svxlink.conf';
-    if (fopen($svxConfigFile,'r'))
-       { $svxconfig = parse_ini_file($svxConfigFile,true,INI_SCANNER_RAW);
-         $refApi = $svxconfig['ReflectorLogic']['API'];
-         $fmnetwork =$svxconfig['ReflectorLogic']['FMNET'];   }
-else { $callsign="N0CALL";
-       $fmnetwork="no registered";
+
+
+        define("KEY1", array(' D1 ','*D1#','green'));
+        define("KEY2", array(' D2 ','*D2#','orange'));
+        define("KEY3", array(' D3 ','*D3#','orange'));
+        define("KEY4", array(' D4 ','*D4#','orange'));
+        define("KEY5", array(' D5', '*D5#','purple'));
+        define("KEY6", array(' D6 ','*D6#','purple'));
+        define("KEY7", array(' D7 ','*D7#','purple'));
+        define("KEY8", array(' D8 ','*D8#','blue'));
+        define("KEY9", array(' D9 ','*D9#','blue'));
+        define("KEY10", array(' D10 ','*D10#','red'));
+        // additional DTMF keys
+        define("KEY11", array(' D11 ','*D11#','green'));
+        define("KEY12", array(' D12 ','*D12#','orange'));
+        define("KEY13", array(' D13 ','*D13#','orange'));
+        define("KEY14", array(' D14 ','*D14#','orange'));
+        define("KEY15", array(' D15 ','*D15#','purple'));
+        define("KEY16", array(' D16 ','*D16#','purple'));
+        define("KEY17", array(' D17 ','*D17#','orange'));
+        define("KEY18", array(' D18 ','*D18#','blue'));
+        define("KEY19", array(' D19 ','*D19#','blue'));
+        define("KEY20", array(' D20 ','*D20#','red'));
+        
+
+
+
+        define("SVXCONFPATH", "/etc/svxlink/");
+        define("SVXCONFIG", "svxlink.conf");
+        define("SVXLOGPATH", "/var/log");
+        define("SVXLOGPREFIX","svxlink.log");
         }
-
-
-
+        include_once 'parse_svxconf.php';
+        
 error_reporting(0);
 // Define name of your FM Network
 define("FMNETWORK", $fmnetwork);
@@ -81,4 +104,16 @@ define("SHOWPTT","TRUE");
 define("KEY9", array(' D9 ','D9#','blue'));
 define("KEY10", array(" D0 ","D0#","blue"));
 //
+$svxConfigFile = '/etc/svxlink/svxlink.conf';
+if (fopen($svxConfigFile,'r'))
+   { $svxconfig = parse_ini_file($svxConfigFile,true,INI_SCANNER_RAW);
+     $refApi = $svxconfig['ReflectorLogic']['API'];
+     $fmnetwork =$svxconfig['ReflectorLogic']['FMNET'];
+     $qth = $svxconfig['LocationInfo']['QTH'];
+     $freq = $svxconfig['Rx1']['RX'];
+    $EL_node = $svxconfig['LocationInfo']['LOCATION'];
+    }
+else { $callsign="N0CALL";
+   $fmnetwork="no registered";
+    }
 ?>
