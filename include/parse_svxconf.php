d75822391622cd0b0ade0bf438384b9151fd6416
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2023-01-14 21:02:32 +0000

    m

--- include/parse_svxconf.php
+++ include/parse_svxconf.php
@@ -4,7 +4,7 @@
 // disable all.
 
 if ( (defined('SVXCONFIG')) && (defined('SVXCONFPATH')) ) {$svxConfigFile = SVXCONFPATH."/".SVXCONFIG ; }
-else {$svxConfigFile = trim(substr(shell_exec("grep CFGFILE /etc/default/svxlink"), strrpos(shell_exec("grep CFGFILE /etc/default/svxlink"), "=")+1)); }
+//else {$svxConfigFile = trim(substr(shell_exec("grep CFGFILE /etc/default/svxlink"), strrpos(shell_exec("grep CFGFILE /etc/default/svxlink"), "=")+1)); }
     if (fopen($svxConfigFile,'r'))
        {$svxconfig = parse_ini_file($svxConfigFile,true,INI_SCANNER_RAW);
          $callsign = $svxconfig['ReflectorLogic']['CALLSIGN'];
