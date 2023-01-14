4928a578c2ea1369d14621a3fa2ac767fadb442d
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2023-01-14 22:01:16 +0000

    m

--- include/parse_svxconf.php
+++ include/parse_svxconf.php
@@ -12,16 +12,17 @@ else {$svxConfigFile = trim(substr(shell_exec("grep CFGFILE /etc/default/svxlink
          $check_logics = explode(",",$svxconfig['GLOBAL']['LOGICS']);
     echo ("$check_logics[0]");
          foreach ($check_logics as $logic_key) {
-            if (strpos($logic_key, 'RepeaterLogic') !== false) {
+            if ($check_logics[0]=="RepeaterLogic") {
               // if we work with CTCSS please set REPORT_CTCSS with correct value in svxlink.conf
               $ctcss = $svxconfig['RepeaterLogic']['REPORT_CTCSS'];
               $system_type="IS_DUPLEX"; // if repeater
               $dtmfctrl = $svxconfig['RepeaterLogic']['DTMF_CTRL_PTY']; }
-            else (strpos($logic_key, 'SimplexLogic') !== false);
-              // if we work with CTCSS please set REPORT_CTCSS with correct value in svxlink.conf
-              $ctcss = $svxconfig['SimplexLogic']['REPORT_CTCSS'];
-              $system_type="IS_SIMPLEX"; // if simplex
-              $dtmfctrl = $svxconfig['SimplexLogic']['DTMF_CTRL_PTY']; 
+            if ($check_Logics[0] == 'SimplexLogic') {
+            // if we work with CTCSS please set REPORT_CTCSS with correct value in svxlink.conf
+            $ctcss = $svxconfig['SimplexLogic']['REPORT_CTCSS'];
+            $system_type = "IS_SIMPLEX"; // if simplex
+            $dtmfctrl = $svxconfig['SimplexLogic']['DTMF_CTRL_PTY'];
+        }
          }
          // additional variables need to define in svxlink.conf in stanza [ReflectorLogic]: API, FMNET, TG_URI
          // FMNET - Name of FM-Network
