3df28cb06826d4ff923db3c772069a2c81ab3dc2
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2023-01-14 21:52:38 +0000

    m

--- include/parse_svxconf.php
+++ include/parse_svxconf.php
@@ -10,15 +10,16 @@ else {$svxConfigFile = trim(substr(shell_exec("grep CFGFILE /etc/default/svxlink
          $callsign = $svxconfig['ReflectorLogic']['CALLSIGN'];
          // check if we are a repeater or a simplex system
          $check_logics = explode(",",$svxconfig['GLOBAL']['LOGICS']);
+    echo ("$check_logics");
          foreach ($check_logics as $logic_key) {
             if (strpos($logic_key, 'RepeaterLogic') !== false) {
               // if we work with CTCSS please set REPORT_CTCSS with correct value in svxlink.conf
-              $ctcss = $svxconfig['RepeaterLogic']['#REPORT_CTCSS'];
+              $ctcss = $svxconfig['RepeaterLogic']['REPORT_CTCSS'];
               $system_type="IS_DUPLEX"; // if repeater
               $dtmfctrl = $svxconfig['RepeaterLogic']['DTMF_CTRL_PTY']; }
             else (strpos($logic_key, 'SimplexLogic') !== false);
               // if we work with CTCSS please set REPORT_CTCSS with correct value in svxlink.conf
-              $ctcss = $svxconfig['SimplexLogic']['#REPORT_CTCSS'];
+              $ctcss = $svxconfig['SimplexLogic']['REPORT_CTCSS'];
               $system_type="IS_SIMPLEX"; // if simplex
               $dtmfctrl = $svxconfig['SimplexLogic']['DTMF_CTRL_PTY']; 
          }
