2dbcbd7cf96422870e9a806739f1f040a80b0f5e
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2023-01-15 19:37:37 +0000

    Revert "m"
    This reverts commit edc214e3ab321221f4ce5ed4935c377a73a4f151.

--- include/parse_svxconf.php
+++ include/parse_svxconf.php
@@ -12,21 +12,25 @@ else {$svxConfigFile = trim(substr(shell_exec("grep CFGFILE /etc/default/svxlink
          $check_logics = explode(",",$svxconfig['GLOBAL']['LOGICS']);
   
             foreach ($check_logics as $key) {
-              
             if ($check_logics[0]=="RepeaterLogic") {
               // if we work with CTCSS please set REPORT_CTCSS with correct value in svxlink.conf
               $ctcss = $svxconfig['RepeaterLogic']['REPORT_CTCSS'];
               $system_type="IS_DUPLEX"; // if repeater
-              $dtmfctrl = $svxconfig['RepeaterLogic']['DTMF_CTRL_PTY'];
-              $modules = $svxconfig['MODULES'];
-            }
+              $dtmfctrl = $svxconfig['RepeaterLogic']['DTMF_CTRL_PTY']; }
             if ($check_Logics[0] == "SimplexLogic") {
             // if we work with CTCSS please set REPORT_CTCSS with correct value in svxlink.conf
+<<<<<<< HEAD
               $ctcss = $svxconfig['SimplexLogic']['REPORT_CTCSS'];
               $system_type = "IS_SIMPLEX"; // if simplex
               $dtmfctrl = $svxconfig['SimplexLogic']['DTMF_CTRL_PTY'];
               $modules = $svxconfig['MODULES'];
             }
+=======
+            $ctcss = $svxconfig['SimplexLogic']['REPORT_CTCSS'];
+            $system_type = "IS_SIMPLEX"; // if simplex
+            $dtmfctrl = $svxconfig['SimplexLogic']['DTMF_CTRL_PTY'];
+        }
+>>>>>>> parent of edc214e (m)
          }
          // additional variables 
          // FMNET - Name of FM-Network
