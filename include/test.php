a88c5777d3eb6b4926de1204e426c0f21fca51fb
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2023-01-18 21:23:35 +0000

    m

--- include/test.php
+++ include/test.php
@@ -17,9 +17,11 @@ if(fopen($nodeInfoFile,'r'))
 {
 	$filedata = file_get_contents($nodeInfoFile); //data read from json file
 	print_r($filedata);
+	echo ("stage one \n");
 	$nodeInfo = json_decode($filedata,true);  //decode a data
 
 	print_r($nodeInfo); //array format data printing
+	echo ("stage two \n");
 	echo build_ini_string(array($nodeInfo));
 
 	 $message = "<h3 class='text-success'>JSON file data</h3>";
