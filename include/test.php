dc0d2ae1e5e781f534fe66690e0a7b0e84542538
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2023-01-18 12:42:08 +0000

    m

--- include/test.php
+++ include/test.php
@@ -6,10 +6,8 @@
 exec("cd ~");
 
 $jsonobj = '/etc/svxlink/node_info.json';
-echo $jsonobj;
-$file = fopen($jsonobj, "r");
+$file = fopen($jsonobj, 'r');
 while (!feof($file)) {
-
     $line_of_text = fgets($file);
     $members = explode('\n', $line_of_text);
 }
