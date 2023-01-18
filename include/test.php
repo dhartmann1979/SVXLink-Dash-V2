01186b0d378c7136e0a18aace58428ef70213556
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2023-01-18 13:30:49 +0000

    m

--- include/test.php
+++ include/test.php
@@ -15,20 +15,12 @@ if(file_exists($nodeInfoFile))
     print_r($filedata);
 	$nodeInfo = json_decode($filedata,true);
     print_r($nodeInfo);
- echo build_ini_string(array($nodeInfo));
+ 	build_ini_string(array($nodeInfo));
 
 };
-	echo $nodeInfo[0];
-	echo $nodeInfo[1];
-	echo $nodeInfo[2];
-	echo $nodeInfo[3];
-	echo $nodeInfo[4];
-	echo $nodeInfo[5];
-	echo $nodeInfo[6];
-	echo $nodeInfo[7];
-	echo $nodeInfo[8];
+	var_dump($nodeInfo);
 
-	 $message = "<h3 class='text-success'>JSON file data</h3>";
+	echo "<h3 class='text-success'>JSON file data</h3>";
 }
 ?>
 
