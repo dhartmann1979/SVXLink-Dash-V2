321d6ee19fb49a47a30c22a596d8cc9a48ea957b
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2023-01-18 11:00:09 +0000

    m

--- nodeInfo/index.php
+++ nodeInfo/index.php
@@ -79,7 +79,7 @@ if (fopen($nodeInfoFile,'r'))
 	$filedata = file_get_contents($nodeInfoFile);
     //print_r($filedata);
 	$nodeInfo = json_decode($filedata,true);
-    print_r($nodeInfo);
+    //print_r($nodeInfo);
 	build_ini_string(array($nodeInfo));
 
 };
