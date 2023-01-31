87cdc18d4367ad67653eef71a5a939429eefa089
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2023-01-31 13:11:58 +0000

    m

--- include/test2.php
+++ include/test2.php
@@ -1,21 +1,25 @@
 <html>
     <body>
     <?php
-$getjson = file_get_contents("/etc/svxlink/node_info.json");
-$getjson_array = json_decode($getjson, true);
-function traverseGeoJSON($iterator) {
-    while ( $iterator -> valid() ) {
-        if ($iterator->hasChildren())
-            traversegeoJSON($iterator->getChildren());
-        elseif ($iterator->key() === "name")
-            echo $iterator->current() . PHP_EOL;
-        $iterator->next();
-        }
-}
-foreach($geoJSON_array as $location)
+$json = file_get_contents("/etc/svxlink/node_info.json");
+$json_array = json_decode($json, true);
+foreach($json_array as $id=>$para)
 {
-    $iterator = new RecursiveArrayIterator($location);
-    traverseGeoJSON($iterator);
+    foreach($para as $key=>$value)
+    {
+        if(is_array($value))
+        {
+            foreach($value as $subkey=>$subvalue)
+            {
+                    echo $subkey . " : " . $subvalue . "\n";
+            }
+        }
+        else
+        {
+                echo $key . " : " . $value . "\n";
+        }
+    }
+        echo "=================================\n";
 }
 ?>
 </body>
