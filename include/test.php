4fb7a100ed83735a18a88970e572ef7f1c3b8ace
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2023-01-31 11:50:27 +0000

    m

--- include/test.php
+++ include/test.php
@@ -18,7 +18,16 @@ echo $nodeInfoFile;
 
 $object=file_get_contents($nodeInfoFile);
 $decoded = json_decode($object, true);
-var_dump($decoded);  ?>
-
+?>
+<td><?$decoded['nodeLocation']['hidden']['sysop']['qth']['name'] ; ?>;</td>
+<td><?$decoded['pos']['lat']['long']['loc']; ?></td>
+<td><?$decoded['rx']['K']['name']['freq']['sqlType']['ant']['comment']['height']['dir']; ?> </td>
+<td><?decoded['tx']['K']['name']['freq']['pwr']['ant']['comment']['height']['dir']['gain']['Antenna_Type']; ?></td>
+<table>
+<tr> Location : ?nodeLocation?  SYSOP : ?sysop?</tr>
+<tr> Coordinates : ?lat? : ?long?   </tr>
+<tr> Receiver : ?rx? : ?freq? : ?ant?</tr>
+<tr> Transmitter : ?tx? : ?name? : ?freq? MHz: ?Antenna_Type? : ?pwr? Watts</tr>
+</table>
 </body>
 </html>
