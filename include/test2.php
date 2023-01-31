5cc8a8cc2e7d39c03bf49c2bc9d4c621e3ccd12f
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2023-01-31 13:51:47 +0000

    m

--- include/test2.php
+++ include/test2.php
@@ -3,6 +3,11 @@
     <?php
 $json = file_get_contents("/etc/svxlink/node_info.json",false);
 $json_array = json_decode($json, true);
+foreach($json_array as $para) {
+    $iterator = new RecursiveArrayIterator($para);
+    traverseStructure($iterator);
+    echo "================\n";
+}
 function traverseStructure($iterator){
     while ( $iterator -> valid()){
         if ($iterator -> hasChildren()) {
@@ -14,11 +19,7 @@ function traverseStructure($iterator){
             $iterator->next();
     }
 }
-    foreach($json_array as $para) {
-            $iterator = new RecursiveArrayIterator($para);
-            traverseStructure($iterator);
-            echo "================\n";
-    }
+
 
 ?>
 </body>
