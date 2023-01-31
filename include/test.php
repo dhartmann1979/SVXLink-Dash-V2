fa00f32d21623cb82f8a61c597c67d1380f2a5c6
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2023-01-31 12:11:30 +0000

    m

--- include/test.php
+++ include/test.php
@@ -18,19 +18,19 @@ $nodeInfoFile = SVXCONFPATH . nodeInfo;
 $object=file_get_contents($nodeInfoFile);
 $decoded = json_decode($object, true);
 ?>
-<td><?= $decoded['nodeLocation']['hidden']['sysop']['qth']['name'] ; ?>;</td>
+<!--<td><?= $decoded['nodeLocation']['hidden']['sysop']['qth']['name'] ; ?>;</td>
 <td><?= $decoded['pos']['lat']['long']['loc']; ?></td>
 <td><?= $decoded['rx']['K']['name']['freq']['sqlType']['ant']['comment']['height']['dir']; ?> </td>
-<td><?= $decoded['tx']['K']['name']['freq']['pwr']['ant']['comment']['height']['dir']['gain']['Antenna_Type']; ?></td>
+<td><?= $decoded['tx']['K']['name']['freq']['pwr']['ant']['comment']['height']['dir']['gain']['Antenna_Type']; ?></td>-->
 <table>
-<tr> Location : <td><?= $decoded['nodeLocation']; ?> SYSOP : <?= $decoded['sysop']; ?></td> </tr>
-<tr> Coordinates : <td><?= $decoded['pos']['lat']['long']['loc']; ?></td>
+<tr> <th>Location : </th><td><?= $decoded['nodeLocation']; ?> SYSOP : <?= $decoded['sysop']; ?></td> </tr>
+<tr> <th>Coordinates : </th> <td><?= $decoded['pos']['lat']['long']['loc']; ?></td>
 </tr>
 
-<tr> Receiver : <td><?= $decoded['rx']['K']['name']['freq']['sqlType']['ant']['comment']['height']['dir']; ?> </td>
+<tr><th> Receiver : </th><td><?= $decoded['rx']['K']['name']['freq']['sqlType']['ant']['comment']['height']['dir']; ?> </td>
 </tr>
 
-<tr> Transmitter : <td><?= $decoded['tx']['K']['name']['freq']['pwr']['ant']['comment']['height']['dir']['gain']['Antenna_Type']; ?></td>
+<tr><th> Transmitter : </th><td><?= $decoded['tx']['K']['name']['freq']['pwr']['ant']['comment']['height']['dir']['gain']['Antenna_Type']; ?></td>
 </tr>
 </table>
 </body>
