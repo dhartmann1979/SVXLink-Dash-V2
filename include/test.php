12797752c6a3196f69102a22585602be8cb46bd0
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2023-01-31 11:17:43 +0000

    m

--- include/test.php
+++ include/test.php
@@ -7,18 +7,6 @@
 
 <?php
 //include_once "include/functions.php";
-function printobj($o){
-  foreach($o as $p=>$v){
-    if(is_object($v)){
-     printobj($v);
-    }elseif(is_array($v)){
-     printobj(json_decode(json_encode($v)));
-    }else{
-     print $p." = ".$v."\n";
-    }
-  }
-return;
-}
 
 
 
@@ -28,25 +16,9 @@ define("nodeInfo", "node_info.json");
 $nodeInfoFile = SVXCONFPATH.nodeInfo;
 echo $nodeInfoFile;
 
-$object=json_decode($nodeInfoFile,true);
-echo $object;
-echo printobj($object);
-/*
-if(fopen($nodeInfoFile,'r'))
-{
-	$filedata = file_get_contents($nodeInfoFile); //data read from json file
-	print_r($filedata);
-	echo ("stage one \n");
-	$nodeInfo = json_decode($filedata,true);  //decode a data
+$object=file_get_contents($nodeInfoFile);
+var_dump($object);
 
-	print_r($nodeInfo); //array format data printing
-	echo ("stage two \n");
-	echo build_ini_string(array($nodeInfo));
-
-	 $message = "<h3 class='text-success'>JSON file data</h3>";
-} else
-	echo "not found";
-*/
     ?>
 
 </body>
