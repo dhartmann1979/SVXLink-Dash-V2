c9a9a5eff157463893fb0e37f906a3a24927cefc
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2023-01-31 12:53:12 +0000

    m

---
+++ include/test2.php
@@ -0,0 +1,18 @@
+<?php
+$getjson = file_get_contents("/etc/svxlink/node_info.json");
+$getjson_array = json_decode($getjson, true);
+function traverseGeoJSON($iterator) {
+    while ( $iterator -> valid() ) {
+        if ($iterator->hasChildren())
+            traversegeoJSON($iterator->getChildren());
+        elseif ($iterator->key() === "name")
+            echo $iterator->current() . PHP_EOL;
+        $iterator->next();
+        }
+}
+foreach($geoJSON_array as $location)
+{
+    $iterator = new RecursiveArrayIterator($location);
+    traverseGeoJSON($iterator);
+}
+?>
