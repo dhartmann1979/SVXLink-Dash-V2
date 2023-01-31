80990dd99d5b95a054f842df2b99a2a89088dec5
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2023-01-31 13:55:27 +0000

    m

--- include/test2.php
+++ include/test2.php
@@ -2,9 +2,9 @@
     <body>
     <?php
 $json = file_get_contents("/etc/svxlink/node_info.json",false);
-    echo '$json';
+    echo $json;
 $json_array = json_decode($json, true);
-    echo '$json_array';
+    echo $json_array;
 foreach($json_array as $para) {
     $iterator = new RecursiveArrayIterator($para);
         echo '$iterator';
