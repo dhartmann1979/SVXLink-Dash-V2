43900bf93058f416478b989b034e6080fc80aa86
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2023-02-03 10:18:16 +0000

    m

--- include/test.php
+++ include/test.php
@@ -10,14 +10,14 @@
   <input type="submit" value="Submit">
 </form>
 
-<p>Click the "Submit" button and the form-data will be sent to a page on the 
-server called "action_page.php".</p>
+<p>click 'submit' </p>
 
 </body>
 </html>
 
 <?php
-exec("cp /etc/svxlink/svxlink.conf /var/www/html/include/svxlink.txt");
+exec("sudo cp /etc/svxlink/svxlink.conf /var/www/html/include/svxlink.txt");
+exec("echo COMPLETE");
 if(isset($_POST['filename'])) $var=$_POST['filename'];
 $a=file_get_contents($var,false);
 echo($a);
