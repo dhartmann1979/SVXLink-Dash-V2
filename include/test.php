f14bbc7d945c6cc1ef8c424240c7f967b6592d96
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2023-02-03 10:40:22 +0000

    m

--- include/test.php
+++ include/test.php
@@ -2,14 +2,11 @@
 <html>
 <body>
 
-<h1>The input element</h1>
+<h1>Working</h1>
 
-<form action="#" method="post">
-    <input type="text" value="<?php echo $var?>" name="var" />
-    <input type="submit" value="Send" />
-</form>
 
-<p>click 'submit' </p>
+
+
 
 </body>
 </html>
@@ -20,9 +17,10 @@ echo $var;
 
 
 $var= shell_exec("sudo cp /etc/svxlink/svxlink.conf /var/www/html/include/svxlink.txt");
-
+echo "<pre>$var</pre>";
+$filename = "include/svxlink.txt";
 //if(isset($_POST['filename'])) $var=$_POST['filename'];
-$output = fopen($var, '+');
+$output = fopen($filename, '+');
 print_r(fgets($output, true));
 ?>
 </html>
