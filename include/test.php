2af87fecc89ba86036e7c563b30ec8298a7b06c4
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2023-02-03 10:45:56 +0000

    m

--- include/test.php
+++ include/test.php
@@ -17,9 +17,15 @@
 
 $var= shell_exec("sudo cp /etc/svxlink/svxlink.conf /var/www/html/include/svxlink.txt");
 echo "<pre>$var</pre>";
-$filename = "include/svxlink.txt";
-//if(isset($_POST['filename'])) $var=$_POST['filename'];
-$output = fopen($filename, '+');
-print_r(fgets($output, true));
+$filename = fopen("include/svxlink.txt", "r");
+$members = array();
+
+while (!feof($file)) {
+   $members[] = fgets($filename);
+}
+
+fclose($filename);
+
+var_dump($members);
 ?>
 </html>
