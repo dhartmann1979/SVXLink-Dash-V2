2ffe9ebb1e20e911b9db4375cc74e9ec5890f67b
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2023-02-07 14:31:02 +0000

    m

--- nodeInfo/index.php
+++ nodeInfo/index.php
@@ -66,44 +66,7 @@ textarea {
 
 
 <?php 
-function build_ini_string(array $a) {
-    $out = '';
-    $sectionless = '';
-    foreach($a as $rootkey => $rootvalue){
-        if(is_array($rootvalue)){
-            // find out if the root-level item is an indexed or associative array
-            $indexed_root = array_keys($rootvalue) == range(0, count($rootvalue) - 1);
-            // associative arrays at the root level have a section heading
-            if(!$indexed_root) $out .= PHP_EOL."[$rootkey]".PHP_EOL;
-            // loop through items under a section heading
-            foreach($rootvalue as $key => $value){
-                if(is_array($value)){
-                    // indexed arrays under a section heading will have their key omitted
-                    $indexed_item = array_keys($value) == range(0, count($value) - 1);
-                    foreach($value as $subkey=>$subvalue){
-                        // omit subkey for indexed arrays
-                        if($indexed_item) $subkey = "";
-                        // add this line under the section heading
-                        $out .= "{$key}[$subkey] = $subvalue" . PHP_EOL;
-                    }
-                }else{
-                    if($indexed_root){
-                        // root level indexed array becomes sectionless
-                        $sectionless .= "{$rootkey}[] = $value" . PHP_EOL;
-                    }else{
-                        // plain values within root level sections
-                        $out .= "$key = $value" . PHP_EOL;
-                    }
-                }
-            }
 
-        }else{
-            // root level sectionless values
-            $sectionless .= "$rootkey = $rootvalue" . PHP_EOL;
-        }
-    }
-    return $sectionless.$out;
-}
 
 
 //$ReflectorConfigFile = '/etc/svxlink/svxreflector.conf';
@@ -117,12 +80,12 @@ if (fopen($nodeInfoFile,'r'))
     //print_r($filedata);
 	$nodeInfo = json_decode($filedata,true);
     //print_r($nodeInfo);
-echo build_ini_string(array($nodeInfo));
+    build_ini_string(array($nodeInfo));
 //        print_r($sectionless . $out);
 };
 
 
-
+/*
 if (fopen($ReflectorConfigFile,'r'))
      {
 
@@ -130,7 +93,7 @@ if (fopen($ReflectorConfigFile,'r'))
 };
 
 
-
+*/
 if (isset($_POST['btnSave']))
     {
         $retval = null;
@@ -153,7 +116,7 @@ if (isset($_POST['btnSave']))
 	$nodeInfo["LinkedTo"] = $_POST['inLinkedTo'];
 
 	$jsonNodeInfo = json_encode($nodeInfo);
-	file_put_contents("/var/www/html/nodeInfo/node_info.json", $jsonNodeInfo ,FILE_USE_INCLUDE_PATH);
+	//file_put_contents("/var/www/html/nodeInfo/node_info.json", $jsonNodeInfo ,FILE_USE_INCLUDE_PATH);
 
         $retval = null;
         $screen = null;
