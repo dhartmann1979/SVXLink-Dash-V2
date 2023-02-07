2beda4bb644139796a9b16d8f891942351154820
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2023-02-07 13:26:01 +0000

    m

--- include/test.php
+++ include/test.php
@@ -5,6 +5,13 @@ include_once __DIR__.'/functions.php';
 include_once __DIR__.'/tgdb.php';
 $filename = "/etc/svxlink/node_info.json";    
 ?>
+<body style="background-color: #e1e1e1;font: 11pt arial, sans-serif;">
+<center>
+<fieldset style="border:#3083b8 2px groove;box-shadow:0 0 10px #999; background-color:#f1f1f1; width:555px;margin-top:15px;margin-left:0px;margin-right:5px;font-size:13px;border-top-left-radius: 10px; border-top-right-radius: 10px;border-bottom-left-radius: 10px; border-bottom-right-radius: 10px;">
+<div style="padding:0px;width:550px;background-image: linear-gradient(to bottom, #e9e9e9 50%, #bcbaba 100%);border-radius: 10px;-moz-border-radius:10px;-webkit-border-radius:10px;border: 1px solid LightGrey;margin-left:0px; margin-right:0px;margin-top:4px;margin-bottom:0px;line-height:1.6;white-space:normal;">
+<center>
+<h1 id="node_info" style="color:#00aee8;font: 18pt arial, sans-serif;font-weight:bold; text-shadow: 0.25px 0.25px gray;">Node Info Configurator</h1>
+
 <span style="font-weight: bold;font-size:14px;">Test Activity</span>
 
 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
@@ -24,3 +31,9 @@ $filename = "/etc/svxlink/node_info.json";
   ?></textarea>
   <input type="submit" value="Save">
 </form>
+</center>
+</div>
+</fieldset>
+</center>
+</body>
+
